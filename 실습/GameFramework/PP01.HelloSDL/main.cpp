0ea0c08d8e7ae79335de55c062c0f98e21a025f6
권기석 <soorison@naver.com>
2018-11-08 18:25:24 +0900

    PP03.GameClass

--- 실습/GameFramework/PP01.HelloSDL/main.cpp
+++ 실습/GameFramework/PP01.HelloSDL/main.cpp
@@ -3,69 +3,23 @@
 	Kwon Ki seok
 	main.cpp
 */
+#include "Game.h"
 
-#include <SDL.h>
-
-bool init(const char* title, int xpos, int ypos, int width, int height, int flags);
-void render();
-void update() {}
-void handleEvents() {}
-void clean() {}
-
-bool g_bRunning = false;
-
-SDL_Window* g_pWindow = 0;
-SDL_Renderer* g_pRenderer = 0;
+Game* g_game = 0;		//게임 오브젝트
 
 int main(int argc, char* args[])
 {
-	
-	if (init("Chapter 1 : Setting up SDL",
-		SDL_WINDOWPOS_CENTERED,
-		SDL_WINDOWPOS_CENTERED,
-		640, 480, SDL_WINDOW_SHOWN))
-	{
-		g_bRunning = true;
-	}
-	else
-	{
-		return 1;		//something's wrong
-	}
+	g_game = new Game();
+	g_game->init("Chapter 1", 100, 100, 640, 480, false);
 
-	while (g_bRunning)
+	while (g_game->running())
 	{
-		render();
+		g_game->handleEvents();
+		g_game->update();
+		g_game->render();
 	}
-	//clean up SDL
-	SDL_Quit();
+	g_game->clean();
 	return 0;
-}
-
-bool init(const char* title, int xpos, int ypos, int width, int height, int flags)
-{
-	if (SDL_Init(SDL_INIT_EVERYTHING) >= 0)
-	{
-		g_pWindow = SDL_CreateWindow(title, xpos, ypos, width, height, flags);
-
-		if (g_pWindow != 0)
-		{
-			g_pRenderer = SDL_CreateRenderer(g_pWindow, -1, 0);
-		}
-	}
-	else
-	{
-		return false;	//SDL could not initialize
-	}
-	return true;
-}
-void render()
-{
-	//Set to Black
-	SDL_SetRenderDrawColor(g_pRenderer, 0, 0, 0, 255);
-	//Clear the window to black
-	SDL_RenderClear(g_pRenderer);
-	//show the window
-	SDL_RenderPresent(g_pRenderer);
 }
 
 /*
