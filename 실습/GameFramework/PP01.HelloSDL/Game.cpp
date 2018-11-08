9197ceba02a68fdf46f147f2419a07f462c7a6c7
Kwon-Kiseok <40809467+Kwon-Kiseok@users.noreply.github.com>
2018-11-09 02:58:21 +0900

    PP05.AnimatingSprite

--- 실습/GameFramework/PP01.HelloSDL/Game.cpp
+++ 실습/GameFramework/PP01.HelloSDL/Game.cpp
@@ -13,12 +13,17 @@ bool Game::init(const char* title, int xpos, int ypos,
 		}
 		m_bRunning = true;
 
-		SDL_Surface* pTempSurface = SDL_LoadBMP("Assets/rider.bmp");
+		SDL_Surface* pTempSurface = SDL_LoadBMP("Assets/animate.bmp");
 		m_pTexture = SDL_CreateTextureFromSurface(m_pRenderer, pTempSurface);
 		SDL_FreeSurface(pTempSurface);
 
-		SDL_QueryTexture(m_pTexture, NULL, NULL,
-			&m_sourceRectangle.w, &m_sourceRectangle.h);
+		m_sourceRectangle.w = 128;
+		m_sourceRectangle.h = 82;
+
+		m_destinationRectangle.x = m_sourceRectangle.x = 0;
+		m_destinationRectangle.y = m_sourceRectangle.y = 0;
+		m_destinationRectangle.w = m_sourceRectangle.w;
+		m_destinationRectangle.h = m_sourceRectangle.h;
 	}
 	else
 	{
@@ -29,11 +34,6 @@ bool Game::init(const char* title, int xpos, int ypos,
 
 void Game::render()
 {
-	m_destinationRectangle.x = m_sourceRectangle.x = 0;
-	m_destinationRectangle.y = m_sourceRectangle.y = 0;
-	m_destinationRectangle.w = m_sourceRectangle.w;
-	m_destinationRectangle.h = m_sourceRectangle.h;
-
 	SDL_RenderClear(m_pRenderer);		//draw color로 render 지우기
 	SDL_RenderCopy(m_pRenderer, m_pTexture,
 		&m_sourceRectangle, &m_destinationRectangle);
@@ -62,4 +62,9 @@ void Game::handleEvents()
 			break;
 		}
 	}
+}
+
+void Game::update()
+{
+	m_sourceRectangle.x = 128 * int(((SDL_GetTicks() / 100) % 6));
 }
