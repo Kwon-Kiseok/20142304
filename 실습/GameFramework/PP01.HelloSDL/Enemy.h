9c481feba7d8c56ecc3004f057c508f7f5f7e07f
Kwon-Kiseok <40809467+Kwon-Kiseok@users.noreply.github.com>
2018-11-09 12:21:23 +0900

    PP10.Polymorphism (virtual 처리)

--- 실습/GameFramework/PP01.HelloSDL/Enemy.h
+++ 실습/GameFramework/PP01.HelloSDL/Enemy.h
@@ -4,9 +4,8 @@
 class Enemy : public GameObject
 {
 public:
-	void load(int x, int y, int width, int height,
-		std::string textureID);
-	void draw(SDL_Renderer* pRenderer);
+	//void load(int x, int y, int width, int height, std::string textureID);
+	//void draw(SDL_Renderer* pRenderer);
 	void update();
-	void clean();
+	//void clean();
 };
